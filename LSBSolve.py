36970204bf5ce68c62c1c3da51ad139493b65206
Zeyshi <tohar.braun@gmail.com>
2017-07-09 20:15:19 +0300

    Update LSBSolve.py

--- LSBSolve.py
+++ LSBSolve.py
@@ -4,8 +4,8 @@ def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
     bits = bin(int(binascii.hexlify(text), 16))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-
-lef = open('EDA4A23F.bmp', 'rb')
+input_file = 'EDA4A23F.bmp'
+lef = open(input_file, 'rb')
 lefile = lef.read()
 LSBs = ''
 for i in range(0, len(lefile)):
@@ -18,4 +18,6 @@ while i < len(LSBs):
 	except:
 		pass
 	i += 8
+with open('output.txt', 'w') as output_file:
+	output_file.write(message)
 print message
